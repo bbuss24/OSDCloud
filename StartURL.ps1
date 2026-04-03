f8f84f34a3ba84f3aebb1b2163fab11f1f3ff89a
bbuss24 <brock.buss@gmail.com>
2026-04-03 11:21:37 -0600

    Update StartURL.ps1

--- StartURL.ps1
+++ StartURL.ps1
@@ -1,1 +1,1 @@
-Start-OSDCloudCLI -OSName 'Windows 11 25H2 x64' -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume
+Start-OSDCloudCLI -OSName 'Windows 11 25H2 x64' -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume -Restart
