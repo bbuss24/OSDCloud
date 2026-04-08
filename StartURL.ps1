94e781ea9c951e8873747408cf59cf9292dc61fe
bbuss24 <brock.buss@gmail.com>
2026-04-08 13:20:35 -0600

    Update OS version from 23H2 to 24H2

--- StartURL.ps1
+++ StartURL.ps1
@@ -1,1 +1,1 @@
-Start-OSDCloudCLI -OSName 'Windows 11 23H2 x64' -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume -Shutdown
+Start-OSDCloudCLI -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume -Shutdown
