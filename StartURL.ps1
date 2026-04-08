c047ead8392d0470a85ade47e7e7e2e642d93a6d
bbuss24 <brock.buss@gmail.com>
2026-04-08 14:34:40 -0600

    Update StartURL.ps1

--- StartURL.ps1
+++ StartURL.ps1
@@ -1,2 +1,1 @@
-#These settings will download Windows 11 24H2 x64 and install the Enterprise Volume License version.
-Start-OSDCloudCLI -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume -Shutdown
+Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume
