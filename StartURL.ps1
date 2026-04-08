67157aa73e837aa46fe945ff7b71a7cf062a1cd8
bbuss24 <brock.buss@gmail.com>
2026-04-08 13:50:16 -0600

    Add comment explaining script functionality
    Added comment to clarify the purpose of the script.

--- StartURL.ps1
+++ StartURL.ps1
@@ -1,1 +1,2 @@
+#These settings will download Windows 11 24H2 x64 and install the Enterprise Volume License version.
 Start-OSDCloudCLI -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Enterprise -OSActivation Volume -Shutdown
